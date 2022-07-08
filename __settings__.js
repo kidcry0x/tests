456f10c51fecb0b28b4ae4a7add88d3a9235e7f1
Tran Quang Minh <kidcry0x@gmail.com>
2022-07-08 16:15:53 +0700

    22

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1467173.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 92483599, 92483518, 92483630, 92483636, 92483501, 92483500, 92483635, 92483524, 92483638, 92483637, 92483640 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/92483535/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/92483503/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/92483533/1/ammo.js', 'preload' : true},
+];
